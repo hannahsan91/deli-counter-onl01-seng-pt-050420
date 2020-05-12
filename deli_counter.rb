87e1e599189c79ce3b361d3626d9a1ea7081a4c8
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 01:49:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
+katz_deli = []
+
 def line(katz_deli)
-  line_positions = []
+  line_positions = [];
   if katz_deli.empty?
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     number = 1
     katz_deli.each do |customer|
@@ -12,7 +14,12 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli)
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
